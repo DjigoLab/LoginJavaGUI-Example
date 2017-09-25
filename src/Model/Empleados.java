2fec444b39d78774074a0e42afe05759e2cd7b4a
Djigo <djigolab@gmail.com>
2017-09-26 00:02:19 +0100

    first
    Here we go

---
+++ src/Model/Empleados.java
@@ -0,0 +1,17 @@
+package Model;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AlumnoDAM
+ */
+public class Empleados {
+    
+    String dni,nombre,apellidos,privilegio;
+    
+}
---
+++ src/loginempleados/LoginEmpleados.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginempleados;
+
+import View.viewLogin;
+
+/**
+ *
+ * @author AlumnoDAM
+ */
+public class LoginEmpleados {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        viewLogin view = new viewLogin();
+        view.setVisible(true);
+        view.setLocationRelativeTo(null);
+    }
+    
+}
